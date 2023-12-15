dc9fc59f3861543890a376597a83683fd2b54d4f
Lindsey Mraz <68626727+lindseymraz@users.noreply.github.com>
2023-12-15 03:09:40 -0500

    Day 13 Part 1

---
+++ src/Day13.java
@@ -0,0 +1,143 @@
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Day13 {
+
+    static final String realPath = "Day13.txt";
+    static final String testPath1 = "src/tests/Day13Part1Test.txt";
+    static final String testPath2 = "src/tests/Day13Part1Test2.txt";
+
+    static String Path = realPath;
+    static int patternsInFile = 0;
+
+    static int day13part1() throws IOException {
+        String[][] patterns = makePatternsArray();
+        int toReturn = 0;
+        for (String[] pattern : patterns) {
+            toReturn += linesLeftOfVerticalLineOfReflection(pattern) + (100 * rowsAboveHorizontalLineOfReflection(pattern));
+        }
+        return toReturn;
+    }
+    static int countPatternsInFile() throws IOException {
+        Scanner scanner = new Scanner(Paths.get(Path));
+        int patterns = 1;
+        while(scanner.hasNextLine()) {
+            if(scanner.nextLine().isEmpty()) {
+                patterns++;
+            }
+        }
+        scanner.close();
+        return patterns;
+    }
+
+    static int[] makeLinesInPatternsArray() throws IOException {
+        Scanner scanner = new Scanner(Paths.get(Path));
+        setPatternsInFile();
+        int[] linesInPatterns = new int[patternsInFile];
+        int pattern = 0;
+        int lines = 0;
+        while(scanner.hasNextLine()) {
+            while(scanner.hasNextLine() && !scanner.nextLine().isEmpty()) {
+                lines++;
+            }
+            linesInPatterns[pattern] = lines;
+            lines = 0;
+            pattern++;
+        }
+        scanner.close();
+        return linesInPatterns;
+    }
+
+    static String[][] makePatternsArray() throws IOException {
+        int[] linesInPatterns = makeLinesInPatternsArray();
+        String[][] patterns = new String[patternsInFile][];
+        Scanner scanner = new Scanner(Paths.get(Path));
+        for (int i = 0; i < linesInPatterns.length; i++) {
+            String[] pattern = new String[linesInPatterns[i]];
+            for (int j = 0; j < linesInPatterns[i]; j++) {
+                if(scanner.hasNextLine()) {
+                    String line = scanner.nextLine();
+                    if(line.isEmpty() && scanner.hasNextLine()) {
+                        line = scanner.nextLine();
+                    }
+                    pattern[j] = line;
+                }
+            } patterns[i] = pattern;
+        }
+        return patterns;
+    }
+
+    static void setPatternsInFile() throws IOException {
+        patternsInFile = countPatternsInFile();
+    }
+
+    static boolean linesMirror(String string1, String string2) {
+        int length = string1.length();
+        for(int i = 0; i < length; i++) {
+            if(!string1.substring(i, i + 1).equals(string2.substring(length - i - 1, length - i))) {
+                return false;
+            }
+        } return true;
+    }
+
+    static int linesLeftOfVerticalLineOfReflection(String patternLines[]) {
+        String string1 = "";
+        String string2 = "";
+        String substring = "";
+        int lines = patternLines.length;
+        int length = patternLines[0].length();
+        for (int i = 0; i < length - 1; i++) {
+            substring = patternLines[0].substring(i, i + 2);
+            if (substring.equals("..") || substring.equals("##")) {
+                int lineOfReflectionIndex = i + 1;
+                int mirroredLines = 0;
+                for (int j = 0; j < lines; j++) {
+                    string1 = patternLines[j].substring(0, lineOfReflectionIndex);
+                    string2 = patternLines[j].substring(lineOfReflectionIndex, length);
+                    if (string1.length() > string2.length()) {
+                        string1 = patternLines[j].substring(lineOfReflectionIndex - string2.length(), lineOfReflectionIndex);
+                    } else {
+                        string2 = patternLines[j].substring(lineOfReflectionIndex, lineOfReflectionIndex + string1.length());
+                    }
+                    if (linesMirror(string1, string2)) {
+                        mirroredLines++;
+                    } else {
+                        break;
+                    }
+                }
+                if (mirroredLines == lines) {return lineOfReflectionIndex;}
+            }
+        }
+        return 0;
+    }
+
+    static boolean checkReflects(String patternLines[], int line1index, int line2index) {
+        int limit = patternLines.length - 1 - line2index;
+        if(limit > line1index) { limit = line1index; }
+        for(int i = 0; i < limit; i++) {
+            if(!patternLines[line1index - (i + 1)].equals(patternLines[line2index + (i + 1)])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static int rowsAboveHorizontalLineOfReflection(String[] patternLines) {
+        String line1 = patternLines[0];
+        String line2 = "";
+        for(int i = 1; i < patternLines.length; i++) {
+            line2 = patternLines[i];
+            if(line1.equals(line2)) {
+                if(checkReflects(patternLines, i-1, i)) {
+                    return i;
+                }
+            }
+            else { line1 = line2; }
+        }
+        return 0;
+    }
+
+}
---
+++ src/tests/Day13Tests.java
@@ -0,0 +1,120 @@
+import org.junit.Test;
+import org.junit.Assert;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.LinkedList;
+
+import static org.junit.Assert.*;
+
+public class Day13Tests {
+    String[] test1 = new String[] {
+            "#.##..##.",
+            "..#.##.#.",
+            "##......#",
+            "##......#",
+            "..#.##.#.",
+            "..##..##.",
+            "#.#.##.#."
+    };
+    String[] test2 = new String[] {
+            "#...##..#",
+            "#....#..#",
+            "..##..###",
+            "#####.##.",
+            "#####.##.",
+            "..##..###",
+            "#....#..#"
+    };
+    String[] test3 = new String[] {
+            "#...##..#",
+            "#....#..#",
+            "..##..###",
+            "#####.##.",
+            "#####.##.",
+            "..##..###"
+    };
+    String[] test4 = new String[] {
+            "#######",
+            "##.....",
+            "##....."
+    };
+    String[] test5 = new String[] {
+            "#.##..",
+            "#.##..",
+            "######"
+    };
+    public Day13Tests() {}
+
+    @Test
+    public void countPatternsInFile() throws IOException {
+        Day13.Path = Day13.testPath1;
+        assertEquals(2, Day13.countPatternsInFile());
+        Day13.Path = Day13.testPath2;
+        assertEquals(3, Day13.countPatternsInFile());
+    }
+
+    @Test
+    public void makeLinesInPatternsArray() throws IOException {
+        Day13.Path = Day13.testPath1;
+        int[] test = new int[]{7, 7};
+        assertArrayEquals(test, Day13.makeLinesInPatternsArray());
+        Day13.Path = Day13.testPath2;
+        int[] test2 = new int[]{7, 6, 3};
+        assertArrayEquals(test2, Day13.makeLinesInPatternsArray());
+    }
+
+    @Test
+    public void makePatternsArray() throws IOException {
+        Day13.Path = Day13.testPath1;
+        String[][] pattern1 = new String[][] {test1, test2};
+        assertArrayEquals(pattern1, Day13.makePatternsArray());
+        Day13.Path = Day13.testPath2;
+        String[][] pattern2 = new String[][] {test1, test3, test4};
+        assertArrayEquals(pattern2, Day13.makePatternsArray());
+    }
+
+    @Test
+    public void linesMirror() {
+        assertTrue(Day13.linesMirror("", ""));
+        assertTrue(Day13.linesMirror(".", "."));
+        assertTrue(Day13.linesMirror("#.", ".#"));
+        assertTrue(Day13.linesMirror(".#", "#."));
+        assertTrue(Day13.linesMirror("###", "###"));
+        assertTrue(Day13.linesMirror("..#", "#.."));
+        assertFalse(Day13.linesMirror("##.", "##."));
+    }
+
+    @Test
+    public void linesLeftOfVerticalLineOfReflection() {
+        assertEquals(5, Day13.linesLeftOfVerticalLineOfReflection(test1));
+        assertEquals(0, Day13.linesLeftOfVerticalLineOfReflection(test2));
+        assertEquals(0, Day13.linesLeftOfVerticalLineOfReflection(test3));
+        assertEquals(1, Day13.linesLeftOfVerticalLineOfReflection(test4));
+        assertEquals(5, Day13.linesLeftOfVerticalLineOfReflection(test5));
+    }
+
+    @Test
+    public void checkReflects() {
+        assertFalse(Day13.checkReflects(test1, 2, 3));
+        assertTrue(Day13.checkReflects(test2, 3, 4));
+        assertTrue(Day13.checkReflects(test3, 3, 4));
+        assertTrue(Day13.checkReflects(test4, 1, 2));
+        assertTrue(Day13.checkReflects(test5, 0, 1));
+    }
+
+    @Test
+    public void rowsAboveHorizontalLineOfReflection() {
+        assertEquals(0, Day13.rowsAboveHorizontalLineOfReflection(test1));
+        assertEquals(4, Day13.rowsAboveHorizontalLineOfReflection(test2));
+        assertEquals(4, Day13.rowsAboveHorizontalLineOfReflection(test3));
+        assertEquals(2, Day13.rowsAboveHorizontalLineOfReflection(test4));
+        assertEquals(1, Day13.rowsAboveHorizontalLineOfReflection(test5));
+    }
+
+    @Test
+    public void day13part1() throws IOException {
+        Day13.Path = Day13.testPath1;
+        assertEquals(405, Day13.day13part1());
+    }
+
+}
